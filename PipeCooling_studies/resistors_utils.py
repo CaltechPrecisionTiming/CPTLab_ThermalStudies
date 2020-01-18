d358d9e526e35857940d39e4f6f2098b1dab0c23
Olmo Cerri <olmo.cerri@gmail.com>
2020-01-17 17:29:38 -0800

    over

--- PipeCooling_studies/resistors_utils.py
+++ PipeCooling_studies/resistors_utils.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-N_resistors = 15
-R0 = 100.0 # Ohms
+# N_resistors = 15
+# R0 = 100.0 # Ohms
 
-def V2power(V):
-    R_tot = R0*N_resistors
+def V2power(V, R0=100.0, N_resistors=15, N_dut=16):
+    R_tot = float(R0*N_resistors)
     power = np.square(V)/R_tot
     I = V/R_tot
-    return power/16.0, I
+    return power/N_dut, I
 
-def power2V(P):
-    P_tot = float(P)*16
+def power2V(P, R0=100.0, N_resistors=15, N_dut=16):
+    P_tot = float(P*N_dut)
     R_tot = R0*N_resistors
     V = np.sqrt(R_tot*P_tot)
     I = V/R_tot
